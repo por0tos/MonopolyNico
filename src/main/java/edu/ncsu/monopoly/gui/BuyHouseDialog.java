a1482199546fa0a1bdd701336f7977089d16f69a
nicoagr <github@nico.eus>
2023-09-29 19:50:53 +0200

    code smell arreglado?

--- src/main/java/edu/ncsu/monopoly/gui/BuyHouseDialog.java
+++ src/main/java/edu/ncsu/monopoly/gui/BuyHouseDialog.java
@@ -15,7 +15,7 @@ import edu.ncsu.monopoly.Player;
 
 
 public class BuyHouseDialog extends JDialog {
-	private JComboBox cboMonopoly; 
+	private JComboBox<String> cboMonopoly; 
 	private JComboBox cboNumber;
 
 	private Player player;
@@ -44,7 +44,7 @@ public class BuyHouseDialog extends JDialog {
 		return btn;
 	}
 
-	private JComboBox buildMonopolyComboBox() {
+	private JComboBox<String> buildMonopolyComboBox() {
 		cboMonopoly = new JComboBox(player.getMonopolies());
 		return cboMonopoly;
 	}
